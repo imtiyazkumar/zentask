214574c8ad86e417db232443df47ef50bca68321
imtiyazkumar <imtiyazah907@gmail.com>
2023-12-01 23:30:50 +0530

    container and cards styled

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
-// import TrashIcon from "../icons/TrashIcon";
-// import { Id, Task } from "../types";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 import { Id, Task } from "./types";
+import { Div, Flex, Img } from "./BaseComponents";
+import { IconMenu } from "@tabler/icons-react";
 
 interface Props {
     task: Task;
@@ -11,108 +10,32 @@ interface Props {
     updateTask: (id: Id, content: string) => void;
 }
 
-function TaskCard({ task, deleteTask, updateTask }: Props) {
-    const [mouseIsOver, setMouseIsOver] = useState(false);
-    const [editMode, setEditMode] = useState(true);
+function TaskCard({ task }: Props) {
 
-    const {
-        setNodeRef,
-        attributes,
-        listeners,
-        transform,
-        transition,
-        isDragging,
-    } = useSortable({
-        id: task.id,
-        data: {
-            type: "Task",
-            task,
-        },
-        disabled: editMode,
-    });
-
-    const style = {
-        transition,
-        transform: CSS.Transform.toString(transform),
-    };
-
-    const toggleEditMode = () => {
-        setEditMode((prev) => !prev);
-        setMouseIsOver(false);
-    };
+    const { setNodeRef, attributes, listeners, transform, transition, isDragging, } = useSortable({ id: task.id, data: { type: "Task", task } });
+    const style = { transition, transform: CSS.Transform.toString(transform) };
 
     if (isDragging) {
         return (
-            <div
-                ref={setNodeRef}
-                style={style}
-                className="
-        opacity-30
-      bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl border-2 border-rose-500  cursor-grab relative
-      "
-            />
-        );
-    }
-
-    if (editMode) {
-        return (
-            <div
-                ref={setNodeRef}
-                style={style}
-                {...attributes}
-                {...listeners}
-                className="bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose-500 cursor-grab relative"
-            >
-                <textarea
-                    className="
-        h-[90%]
-        w-full resize-none border-none rounded bg-transparent text-white focus:outline-none
-        "
-                    value={task.content}
-                    autoFocus
-                    placeholder="Task content here"
-                    onBlur={toggleEditMode}
-                    onKeyDown={(e) => {
-                        if (e.key === "Enter" && e.shiftKey) {
-                            toggleEditMode();
-                        }
-                    }}
-                    onChange={(e) => updateTask(task.id, e.target.value)}
-                />
-            </div>
+            <div ref={setNodeRef} style={style} className=" p-2 h-[100px] min-h-max items-center flex rounded-md border border-primary cursor-grab" />
         );
     }
 
     return (
-        <div
-            ref={setNodeRef}
-            style={style}
-            {...attributes}
-            {...listeners}
-            onClick={toggleEditMode}
-            className="bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose-500 cursor-grab relative task"
-            onMouseEnter={() => {
-                setMouseIsOver(true);
-            }}
-            onMouseLeave={() => {
-                setMouseIsOver(false);
-            }}
-        >
-            <p className="my-auto h-[90%] w-full overflow-y-auto overflow-x-hidden whitespace-pre-wrap">
-                {task.content}
-            </p>
-
-            {mouseIsOver && (
-                <button
-                    onClick={() => {
-                        deleteTask(task.id);
-                    }}
-                    className="absolute p-2 -translate-y-1/2 rounded stroke-white right-4 top-1/2 bg-columnBackgroundColor opacity-60 hover:opacity-100"
-                >
-                    {/* <TrashIcon /> */}
-                    trash
-                </button>
-            )}
+        <div ref={setNodeRef} style={style}{...attributes}{...listeners} className="p-2 bg-orange-200 rounded-md cursor-grab" >
+            <Flex className="gap-3 pb-1">
+                <Div className="bg-red-500 h-[10px] w-12 rounded-md"></Div>
+                <Div className="bg-red-500 h-[10px] w-12 rounded-md"></Div>
+                <Div className="w-6 h-6 p-1 ml-auto bg-blue-500 rounded-full cursor-pointer hover:flex"><IconMenu size={15} /></Div>
+            </Flex>
+            <Div className="py-1 text-justify text-14">{task.content}</Div>
+            <Flex className="justify-end gap-2">
+                {/* {<> */}
+                <Img className="rounded-full w-[25px] h-[25px]" src="https://img.freepik.com/free-photo/forest-landscape_71767-127.jpg?size=626&ext=jpg&ga=GA1.1.1803636316.1701388800&semt=ais" />
+                <Img className="rounded-full w-[25px] h-[25px]" src="https://img.freepik.com/free-photo/forest-landscape_71767-127.jpg?size=626&ext=jpg&ga=GA1.1.1803636316.1701388800&semt=ais" />
+                {/* </>
+                } */}
+            </Flex>
         </div>
     );
 }
